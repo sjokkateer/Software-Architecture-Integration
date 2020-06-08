5c5639a27b0315f787e3b8640663e1d68be8a160
Remy Bos <remy90@hotmail.com>
2020-06-08 22:55:13 +0200

    RECIPIENT LIST: Add dynamic loading of rules

--- travel-refund-broker/src/main/java/routing/RecipientList.java
+++ travel-refund-broker/src/main/java/routing/RecipientList.java
@@ -6,44 +6,36 @@ import gateway.MessageSenderGateway;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecipientList {
-    // This will be part of the TravelApprovalAppGateway, and will communicate through this.
-    // Gets two messageSenders, one to each queue.
-    // Will always send off the approval request to the internship admin
-    // if condition matched also to the financial service.
-    //
     private static final String AGGREGATION_PROPERTY = "aggregationID";
 
-    private MessageSenderGateway administrationSenderGateway;
-    private MessageSenderGateway financeSenderGateway;
-
+    private List<RuleBasedSender> ruleBasedSenders;
 
     public RecipientList() {
-        administrationSenderGateway = new MessageSenderGateway("administrationRequestQueue");
-        financeSenderGateway = new MessageSenderGateway("financeRequestQueue");
+        ruleBasedSenders = RuleBasedSender.loadFromFile();
     }
 
     public Aggregation send(ApprovalRequest approvalRequest, String messageId) {
         Gson gson = new Gson();
-
         String jsonAR = gson.toJson(approvalRequest, ApprovalRequest.class);
-        Message message = administrationSenderGateway.createTextMessage(jsonAR);
 
         Aggregation aggregation = new Aggregation();
 
         try {
-            message.setJMSCorrelationID(messageId);
+            int numberOfMessagesSent = 0;
+            for (RuleBasedSender ruleBasedSender : ruleBasedSenders) {
+                Message message = ruleBasedSender.createTextMessage(jsonAR);
+                message.setJMSCorrelationID(messageId);
+                message.setIntProperty(AGGREGATION_PROPERTY, aggregation.getAggregationId());
 
-            int numberOfMessagesSent = 1;
-            message.setIntProperty(AGGREGATION_PROPERTY, aggregation.getAggregationId());
-
-            if (approvalRequest.getCosts() >= 50) {
-                numberOfMessagesSent++;
-                financeSenderGateway.send(message);
+                if (ruleBasedSender.send(approvalRequest.getCosts(), message)) {
+                    numberOfMessagesSent++;
+                }
             }
 
-            administrationSenderGateway.send(message);
             aggregation.setNumberOfMessages(numberOfMessagesSent);
         } catch (JMSException e) {
             e.printStackTrace();
---
+++ travel-refund-broker/src/main/java/routing/RuleBasedSender.java
@@ -0,0 +1,76 @@
+package routing;
+
+import gateway.MessageSenderGateway;
+import org.mariuszgromada.math.mxparser.*;
+import javax.jms.Message;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RuleBasedSender {
+    private static final String RESOURCE_FOLDER = "/travel-refund-broker/src/main/resources";
+    private static final String RULE_FILE = "rules.txt";
+
+    private static final int RULE = 0;
+    private static final int QUEUE_NAME = 1;
+
+    private MessageSenderGateway gateway;
+    private String rule;
+
+    public RuleBasedSender(MessageSenderGateway gateway, String rule) {
+        this.gateway = gateway;
+        this.rule = rule;
+    }
+
+    public boolean send(double costs, Message message) {
+        String argument = " costs = " + costs;
+
+        Expression expression = new Expression(rule, new Argument(argument));
+        double result = expression.calculate();
+
+        if (result == 1.0) {
+            gateway.send(message);
+            return true;
+        }
+
+        return false;
+    }
+
+    public Message createTextMessage(String content) {
+        return gateway.createTextMessage(content);
+    }
+
+    public static List<RuleBasedSender> loadFromFile() {
+        String pathToFiles = System.getProperty("user.dir");
+        List<RuleBasedSender> ruleBasedSenders = new ArrayList<>();
+
+        if((new File(pathToFiles + RESOURCE_FOLDER)).exists()) {
+            pathToFiles += RESOURCE_FOLDER;
+
+            MessageSenderGateway messageSenderGateway;
+            String rule;
+
+            try {
+                File inputFile = new File(pathToFiles + "/" + RULE_FILE);
+                Scanner scanner = new Scanner(inputFile);
+
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    String[] items = line.split(",");
+
+                    rule = items[RULE];
+                    messageSenderGateway = new MessageSenderGateway(items[QUEUE_NAME]);
+
+                    ruleBasedSenders.add(new RuleBasedSender(messageSenderGateway, rule));
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return ruleBasedSenders;
+    }
+}
